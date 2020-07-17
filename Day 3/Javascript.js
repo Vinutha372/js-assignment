05131521abd7fdcfd7c4f77101e3c5a3f2855124
Vinutha H <68235711+Vinutha372@users.noreply.github.com>
2020-07-17 13:09:34 +0530

    Day 3 assignment

---
+++ Day 3/Javascript.js
@@ -0,0 +1,11 @@
+let Marks=parseInt(prompt("Enter your marks"))
+    let str = "marks";
+    if(marks>=70 && marks<=100){
+        console.log(`marks are ${marks} and Grade is A `)
+      }else if (Marks>=50 && marks<=69) {
+        console.log(`marks are ${marks} and Grade is B `)      
+      }else if (Marks>=40 && marks<=49) {
+        console.log(`marks are ${marks} and Grade is C `) 
+      } else{
+        console.log(`marks are ${marks} and Grade is A `)
+    }
